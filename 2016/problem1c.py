b7bf1380e49f28ed42cdae2d2e4bb5167c7b92f1
Matt Snider <matt.snider@alum.utoronto.ca>
2016-04-09 22:41:43 +0200

    Qual 1c: Remove sieve of eratosthenes and solve small input

--- 2016/problem1c.py
+++ 2016/problem1c.py
@@ -6,19 +6,6 @@ Problem 1C: Coin Jam
 By Matt Snider
 2016-04-09
 """
-prime_sieve = []
-
-
-def sieve_of_eratosthenes(n):
-    sieve = [True] * n
-    sieve[0] = sieve[1] = False
-
-    for i, is_prime in enumerate(sieve):
-        if not is_prime:
-            continue
-        for j in range(i*i, n, i):
-            sieve[j] = False
-    return sieve
 
 
 def jamcoin_candidates(size):
@@ -30,25 +17,25 @@ def jamcoin_candidates(size):
 
 def is_jamcoin(n):
     """Checks if the number n represents a jamcoin"""
-    global prime_sieve
-
     i = 10
     while i >= 2:
         at_base = int(str(n), base=i)
-        if prime_sieve[at_base]:
+        if is_prime(at_base):
             return False
         i -= 1
     return True
 
 
+def is_prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 def find_jamcoins(size, n):
     """Finds n jamcoins of the given size."""
-    global prime_sieve
-
     jamcoins = []
-    if not prime_sieve:
-        prime_sieve = sieve_of_eratosthenes((10**size) // 2)
-
     for candidate in jamcoin_candidates(size):
         if len(jamcoins) == n:
             break
